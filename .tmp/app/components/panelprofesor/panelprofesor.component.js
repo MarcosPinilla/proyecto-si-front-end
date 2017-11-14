c0792342819615cb1458669384dd84111e0f052a
Marcos Pinilla <m.pinilla03@ufromail.cl>
2017-11-14 11:22:02 -0300

    alumno y profesor avance 1

---
+++ .tmp/app/components/panelprofesor/panelprofesor.component.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict';
+
+  angular
+  .module('app')
+  .component('panelprofesor', {
+    templateUrl: 'app/components/panelprofesor/panelprofesor.html',
+    controller: panelprofesorCtrl,
+    controllerAs: 'vm'
+  });
+
+  function panelprofesorCtrl() {}
+})();
